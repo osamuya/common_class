27680a61dc0be983e4c2911e6c014d29119f4415
osamuya <oosamuuy@gmail.com>
2016-11-10 12:13:32 +0900

    チェックデジットを作る

---
+++ www/checkdegit.php
@@ -0,0 +1,15 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Check degit</title>
+</head>
+<body>
+<h1>Check degit</h1>
+</body>
+</html>
+
+
+
---
+++ www/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>
+		common class
+	</title>
+</head>
+<body>
+<h1>common class</h1>
+<ul>
+	<li><a href="checkdegit.php">Checkdegit</a></li>
+</ul>
+</body>
+</html>
