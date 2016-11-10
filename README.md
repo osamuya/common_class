7bbb9a41127757f390246aff27fe605eaa60b359
osamuya <oosamuuy@gmail.com>
2016-11-10 17:38:58 +0900

    チェックデジットをclass化　JAN Short type 8degits

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # common class
 
 phpとJavascriptのよく使う共通関数とかを作ってます。
+
+## 使い方
+
+- wwwをDocumentRootにすると動くと思います。
--- www/checkdegit.php
+++ www/checkdegit.php
@@ -1,13 +1,32 @@
 <?php
-
+require_once (__DIR__."/../lib/common/checkdegit.php");
 ?>
+
 <!DOCTYPE html>
 <html>
 <head>
+	<script
+		src="https://code.jquery.com/jquery-2.2.4.js"
+		integrity="sha256-iT6Q9iMJYuQiMWNd9lDyBUStIq/8PuOW33aOqmvFpqI="
+		crossorigin="anonymous">
+	</script>
 	<title>Check degit</title>
 </head>
+</head>
+
 <body>
 <h1>Check degit</h1>
+頭から7桁の数字のチェックデジットが8桁目（太字）の数字になります。<br>
+14300011←最後の1桁がチェックデジット<br>
+<br>
+<?php
+	$obj = new checkdegit();
+	foreach ($number as $key => $value) {
+		$para = $value[0].$value[1].$value[2];
+		$checkdegit = $obj->janCode_8degits($para);
+		print $para."<strong>".$checkdegit."</strong><br>\n";
+	}
+?>
 </body>
 </html>
 
